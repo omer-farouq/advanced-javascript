a729c35cdef874ad2c233cf0bef25eab285d4ed2
omer-farouq <omerfarouqstd@gmail.com>
2022-08-11 06:12:35 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,40 @@
+//je kono shonkha ০ হলে মিথ্যা ধরবে এবং নাম্বার হলে সত্য ধরবে।
+ 
+// const age = -4;
+
+// if(age){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+//যে  কোন জিনিস জাবাস্ক্রিপটে ডিক্লেয়ার না করা হলে সেটা জাবাস্ক্রিপ্ট False //
+//ধরে নিবে
+
+//স্ট্রিং  এর ক্ষেত্রে শুধু মাত্র এমটি স্টিংই হলো //flase আসবে এবং গুলো সব true আসবে এবং কি হোয়াইট //স্পেস ও
+
+// নাম্বার এর ক্ষেত্রে শুধু মাত্র 0 কে False ধরবে //আর তা বাদে সকল নাম্বারই True হবে।
+
+// ‍স্ট্রিং এর ক্ষেত্রে চেক
+
+// var name = "0";
+//False:
+//0
+//""
+//false howar condition
+// undifind
+//  Nan = Not a number  
+// null
+//false
+//Truthy:
+//"0"
+//object
+//array
+
+let name = 12;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
